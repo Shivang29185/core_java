b1097349283a83fa3edc2877bebdcb79bbfd251c
Shivang29185 <shivangbgohel@gmail.com>
2022-01-28 10:41:04 +0530

    complete 25

--- ComparatorDemo.java
+++ ComparatorDemo.java
@@ -9,28 +9,69 @@ import java.util.Comparator;
 * @author Shivang
 * @version 1.01 2022-01-27
 */
-class NameComparator implements 
-public class ComparatorDemo {
-	
-	public static void main(String[] args) {
-		ArrayList<StudentDetails> list = new ArrayList<StudentDetails>();
-		list.add(new StudentDetails(2, "Shivang", 21));
-		list.add(new StudentDetails(3, "Ravi", 23));
-		list.add(new StudentDetails(1, "Rutvik", 20));
+class Students {
 
-		System.out.println("List before Shorting"); // Printing Before Shorting
-		for (StudentDetails stDetails : list) {
-			System.out.println(stDetails);
-		}
+	int id;
+	int age;
+	String name;
 
-		Collections.sort(list, new NameComparator() ()); // Shorting method
-
-		System.out.println();
-		System.out.println("List After Shorting"); // Printing After Shorting
-		for (StudentDetails stDetails : list) {
-			System.out.println(stDetails);
-		}
-
+	public Students(int id, String name, int age) { // Constructor
+		this.id = id;
+		this.name = name;
+		this.age = age;
 	}
 
-}
+	@Override
+	public String toString() {
+		return "Students [id=" + id + ", age=" + age + ", name=" + name + "]";
+	}
+
+} // End of class
+
+class AgeComparator implements Comparator <Students> {
+
+	@Override
+	public int compare(Students student1, Students student2) { // Method of AgeComparator
+		return student1.age - student2.age;
+	}
+
+} // End of class
+
+public class ComparatorDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Students> list = new ArrayList<Students>();
+		list.add(new Students(2, "Shivang", 21));
+		list.add(new Students(3, "Ravi", 23));
+		list.add(new Students(1, "Rutvik", 20));
+
+		System.out.println("List before Shorting"); // Printing Before Shorting
+		for (Students stDetails : list) {
+			System.out.println(stDetails);
+		}
+
+		Collections.sort(list, new Comparator<Students>() {
+
+			@Override
+			public int compare(Students student1, Students student2) {
+				
+				return student1.name.compareTo(student2.name);
+			}
+		}); // Shorting method
+
+		System.out.println();
+		System.out.println("List After Shorting using Name"); // Printing After Shorting
+		for (Students stDetails : list) {
+			System.out.println(stDetails);
+		}
+
+		Collections.sort(list, new AgeComparator()); // Shorting method
+
+		System.out.println();
+		System.out.println("List After Shorting using age"); // Printing After Shorting
+		for (Students stDetails : list) {
+			System.out.println(stDetails);
+		}
+	} // End of main
+
+} // End of class
